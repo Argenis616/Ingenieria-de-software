0cfef1b7d0ccac6216636d5d0a09c2814575096e
Argenis616 <argenis616@ciencias.unam.mx>
2019-02-19 15:31:11 -0600

    Agregando DAO e interfaz

--- mapita/src/main/java/com/mycompany/modelo/AbstractDAO.java
+++ mapita/src/main/java/com/mycompany/modelo/AbstractDAO.java
@@ -9,61 +9,140 @@ import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-
+import java.util.List;
 /**
  *
  * @author Jorge
  */
-public abstract class AbstractDAO {
-
+public abstract class AbstractDAO<T> {
+    
+    /**
+     * 
+     */
     protected SessionFactory sessionFactory;
-
+    
+    /**
+     * 
+     */
     public AbstractDAO(){
         this.sessionFactory = HibernateUtil.getSessionFactory();
-
     }
-
-    protected void save(Object obj){
+    
+    /**
+     * 
+     * @param obj 
+     */
+    protected void save(T obj){
         Session session = this.sessionFactory.openSession();
         Transaction tx = null;
         try{
             tx = session.beginTransaction();
-
+            session.save(obj);
+            tx.commit();
         }catch(HibernateException e){
-            if(tx != null){
+            if(tx!=null){
                 tx.rollback();
             }
             e.printStackTrace();
-
-        } finally{
+        }finally{
+            session.close();
+        }
+    
+    }
+    
+    /**
+     * 
+     * @param obj 
+     */
+    protected void update(T obj){
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            session.update(obj);
+            tx.commit();
+        }catch(HibernateException e){
+            if(tx!=null){
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }finally{
             session.close();
         }
     }
-
-
-    protected void update(Object obj){}
-
-    protected void delate(Object objt){}
-
-    protected Object find(Class clazz, int id){
-        Object objt = null;
+    
+    /**
+     * 
+     * @param obj 
+     */
+    protected void delete(T obj){
+        Session session = this.sessionFactory.openSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            session.delete(obj);
+            tx.commit();
+        }catch(HibernateException e){
+            if(tx!=null){
+                tx.rollback();
+            }
+            e.printStackTrace();
+        }finally{
+            session.close();
+        }
+    }
+    /**
+     * 
+     * @param clazz
+     * @param id
+     * @return 
+     */
+    protected T find(Class clazz, int id){
+        T obj =null;
         Session session = this.sessionFactory.getCurrentSession();
         Transaction tx = null;
         try{
             tx = session.beginTransaction();
-            String hql = "From" + clazz;
-            Query query = session.createQuery(hql);
-            objt = query.uniqueResult();
+            obj =(T)session.get(clazz, id);
             tx.commit();
+            
         }catch(HibernateException e){
-            if (tx != null){
+            if(tx!=null){
                 tx.rollback();
             }
         }finally{
             session.close();
+        
         }
-        return objt;
+        return obj;
+        
     }
-
-    //protected List<T> findAll(Class clazz){}
+    /**
+     * 
+     * @param clazz
+     * @return 
+     */
+    protected List<T> findAll(Class clazz){
+        List<T> obj =null;
+        Session session = this.sessionFactory.getCurrentSession();
+        Transaction tx = null;
+        try{
+            tx = session.beginTransaction();
+            String hql = "From"+clazz;
+            Query query = session.createQuery(hql);
+            obj = (List<T>)query.list();
+            tx.commit();
+            
+        }catch(HibernateException e){
+            if(tx!=null){
+                tx.rollback();
+            }
+        }finally{
+            session.close();
+        
+        }
+        return obj;
+    }
+    
+    
 }
